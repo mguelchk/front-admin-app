22b30fd3be9ea9e33136d9ec31c01f2e12074f24
GillianCova <gsanchezh.ids@gmail.com>
2019-12-04 14:45:02 -0600

    corrección a estilos y ajustes para el componente sitio.web.

--- src/app/sitio-web/sitio.web.component.ts
+++ src/app/sitio-web/sitio.web.component.ts
@@ -6,13 +6,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sitio-web',
   templateUrl: './sitio.web.component.html',
-  styles: []
+  styleUrls: ['./stylepw.css']
 })
 export class SitioWebComponent implements OnInit {
-
+  inicioActivo: boolean = true;
+  servicioActivo: boolean = false;
+  nosotrosActivo: boolean = false;
+  contactoActivo: boolean = false;
   constructor(
   ) { }
 
   ngOnInit() {
+  }
+  
+  activaBoton(boton:String){
+    this.inicioActivo = (boton == 'i') ;
+    this.servicioActivo = (boton == 's');
+    this.nosotrosActivo = (boton == 'n');
+    this.contactoActivo = (boton == 'c');
   }
 }
